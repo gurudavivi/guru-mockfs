e7e05a7535ff72ce0776ac73ac46efee6cce78af
vivianeflowt <vivianeflowt@gmail.com>
2022-05-18 12:01:20 -0300

    step

--- packages/api/src/config/index.ts
+++
@@ -1,5 +0,0 @@
-import config from './config'
-
-export const configure = () => [config]
-
-export default configure
--- packages/api/src/database/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './database.module'
-export * from './database.setup'
-export * from './ormconfig'
-export * from './snake-naming.strategy'

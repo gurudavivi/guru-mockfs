6c39b57d50dc277a60587761f691591813bb2c08
vivianeflowt <vivianeflowt@gmail.com>
2022-05-19 13:26:42 -0300

    step 1

--- macro.sh
+++ macro.sh
@@ -3,17 +3,30 @@
 clear
 
 
-for i in {1..5}
+for i in {1..100}
 do
-   ts-node utils/library/src/commits.ts
-   sleep 2
-   git add .
+    ts-node utils/library/src/commits.ts
+    sleep 2
+
+    echo "  "
+    git add .
     git commit -m "step $i"
+    echo "  "
+    sleep 1
+
+
+    echo "  "
     git push -f
+    sleep 1
 
-   echo "Welcome $i times"
-   sleep 2
+    echo "  "
+    echo "  "
+    echo "Welcome $i times"
+    echo "  "
+    echo "  "
+    sleep 2
 
-   rm -rf ./data
+    rm -rf ./data
+    sleep 2
 done
 
